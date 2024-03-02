7cc668861af32f16552ba9e668467d97c56f80ed
SandruEzechiel <146414741+SandruEzechiel@users.noreply.github.com>
2024-03-02 13:40:30 +0200

    Add files via upload
    Tema
    2 Martie 2024

---
+++ EX5/src/ClientComplex.java
@@ -0,0 +1,15 @@
+
+public class ClientComplex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Complex g=new Complex (10, 34);
+System.out.println(g.modulComplex(g));
+g.afisareComplex();
+Complex t=new Complex (4,5);
+
+System.out.println(g.suma(t));
+g.numarAfisari();
+	}
+
+}
