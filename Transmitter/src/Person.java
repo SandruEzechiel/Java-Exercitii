1e2f067b1d23760a7982d839d8e1075cf80ce2ba
SandruEzechiel <146414741+SandruEzechiel@users.noreply.github.com>
2024-05-17 23:07:19 +0300

    .

---
+++ Transmitter/src/Person.java
@@ -0,0 +1,26 @@
+
+public class Person {
+
+	private String nume;
+	private Transmitter transmitter;
+	
+	public Person(String nume) {
+		this.nume = nume;
+	}
+	public void setTransmitter(Transmitter transmitter) {
+		this.transmitter=transmitter;
+		
+	}
+	public void send(Person p, String message) {
+		Message m = new Message(this, p, message);
+		transmitter.store(m);
+	}
+	public String getNume() {
+		return nume;
+	}
+	public Transmitter getTransmitter() {
+		return transmitter;
+	}
+	
+	
+}
