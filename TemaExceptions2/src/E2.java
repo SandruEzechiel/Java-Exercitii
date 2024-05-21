56156d8fefe837340ce631ab12d9c9a3f4776ebd
SandruEzechiel <146414741+SandruEzechiel@users.noreply.github.com>
2024-05-21 22:36:31 +0300

    .

---
+++ TemaExceptions2/src/E2.java
@@ -0,0 +1,10 @@
+
+class E2 extends RuntimeException {
+    public E2() {
+        super();
+    }
+
+    public E2(String message) {
+        super(message);
+    }
+}
---
+++ TemaExceptions2/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Exemplu exemplu = new Exemplu();
+        
+        exemplu.doSomething(0);
+        exemplu.doSomething(3);
+    }
+}
